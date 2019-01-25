5983eb3b26c00822278adf5a5d149277324feae7
Karolis Narkevicius <karolis.n@gmail.com>
2019-01-25 12:15:28 +0000

    Not really tested yet :-S

---
+++ README.md
@@ -0,0 +1,26 @@
+# SublimeLinter-contrib-healthier
+
+A linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) to integrate [Healthier Linter](https://github.com/KidkArolis/healthier).
+
+## Installation
+
+SublimeLinter must be installed in order to use this plugin.
+
+Please use [Package Control](https://packagecontrol.io) to install the linter plugin.
+
+Before installing this plugin, you must ensure that `healthier` is installed as your project dependency:
+
+```
+$ npm install healthier
+```
+
+If `healthier` is installed in the project as a dev dependency, the SublimeLinter should have no trouble finding it. If you have issues, make sure to check out [troubleshooting PATH configuration](https://sublimelinter.readthedocs.io/en/latest/troubleshooting.html#finding-a-linter-executable).
+
+## Settings
+
+- [SublimeLinter settings](https://sublimelinter.readthedocs.org/en/latest/settings.html)
+- [Linter settings](https://sublimelinter.readthedocs.org/en/latest/linter_settings.html)
+
+## Note
+
+Healthier linting is only enabled for projects with `healthier` in `devDependencies`/`dependencies` in package.json.
---
+++ linter.py
@@ -0,0 +1,36 @@
+"""This module exports the Healthier plugin class."""
+
+from SublimeLinter.lint import NodeLinter
+import re
+
+class Healthier(NodeLinter):
+    """Provides an interface to healthier."""
+
+    syntax = ('javascript', 'html', 'javascriptnext', 'javascript 6to5', 'javascript (babel)')
+    cmd = 'healthier --stdin --verbose'
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0.0'
+    regex = r'^\s.+:(?P<line>\d+):(?P<col>\d+):(?P<message>.+)'
+    selectors = {
+        'html': 'source.js.embedded.html'
+    }
+
+    html_pattern = re.compile(r'(^.*\n)\s+$', re.DOTALL)
+
+    npm_name = 'healthier'
+    defaults = {
+        'disable_if_not_dependency': True
+    }
+
+    def run(self, cmd, code):
+        """
+        If HTML syntax and the last line is just whitespace then remove it.
+        Its probably just space before closing.
+        script tag
+        """
+        if code and self.syntax == 'html':
+            match = self.html_pattern.match(code)
+            if match:
+                code = match.group(1)
+return super(Healthier, self).run(cmd, code)
