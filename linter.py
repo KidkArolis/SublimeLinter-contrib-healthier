ba61959adf3d89501af490ae6d2e54229c1cfd31
Karolis Narkevicius <karolis.n@gmail.com>
2019-01-26 23:06:15 +0000

    Update code to use latest SublimeLinter API

--- linter.py
+++ linter.py
@@ -1,36 +1,23 @@
 """This module exports the Healthier plugin class."""
 
-from SublimeLinter.lint import NodeLinter
 import re
+from SublimeLinter.lint import NodeLinter
 
 class Healthier(NodeLinter):
     """Provides an interface to healthier."""
 
-    syntax = ('javascript', 'html', 'javascriptnext', 'javascript 6to5', 'javascript (babel)')
     cmd = 'healthier --stdin --verbose'
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 1.0.0'
     regex = r'^\s.+:(?P<line>\d+):(?P<col>\d+):(?P<message>.+)'
-    selectors = {
-        'html': 'source.js.embedded.html'
-    }
-
-    html_pattern = re.compile(r'(^.*\n)\s+$', re.DOTALL)
-
-    npm_name = 'healthier'
     defaults = {
+        'selector': 'source.js - meta.attribute-with-value',
         'disable_if_not_dependency': True
     }
 
+    def on_stderr(self, stderr):
+        if ('healthier: Friendly linter' in stderr):
+            return
+        else:
+            logger.error(stderr)
+
     def run(self, cmd, code):
-        """
-        If HTML syntax and the last line is just whitespace then remove it.
-        Its probably just space before closing.
-        script tag
-        """
-        if code and self.syntax == 'html':
-            match = self.html_pattern.match(code)
-            if match:
-                code = match.group(1)
-        return super(Healthier, self).run(cmd, code)
+        return super().run(cmd, code)
